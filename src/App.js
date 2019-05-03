2461e9b6e410eaac43a764530a04f571fc0e3ea3
mich <843298603@qq.com>
2019-05-03 15:03:41 +0800

    react-redux

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import './App.css';
+class App extends Component {
+
+  render() {
+    return (
+    <div>
+      <span id="value">{this.props.n}</span>
+      <div>
+        <button id="add1" onClick={()=> this.props.add1()}>+1</button>
+        
+      </div>
+    </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    n: state.n
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    add1: ()=>{ dispatch ({type:'add',payload:1})
+    }
+  }
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
